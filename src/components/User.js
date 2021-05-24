bf3fa4704adcf94338e7bda441112e56244e848b
Long Tran <trlong357@gmail.com>
2021-05-24 16:16:51 +0700

    165. Working with State & Event

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,47 @@
 import { Component } from "react";
 
-import classes from "./User.module.css";
+import User from "./User";
+import classes from "./Users.module.css";
 
-class User extends Component {
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+      more: "Test",
+    };
+  }
+
+  toggleUsersHandler() {
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+
   render() {
-    return <li className={classes.user}>{this.props.name}</li>;
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
   }
 }
 
-export default User;
+export default Users;
